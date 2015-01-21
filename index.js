ff3ffc306152356161345c96b6ab3c8ab923d392
Dmitri Mesin <dmitriym@elance-odesk.com>
2015-01-21 18:01:52 +0200

    initial state of favicon extension

---
+++ index.js
@@ -0,0 +1,16 @@
+/**
+ * Setting all the parsers that are used in HTTP protocol for basic case
+ */
+module.exports.extension = function() {
+    "use strict";
+
+    var fs = require('fs')
+        , path = require('path')
+        , faviconFileName = twee.getConfig('twee:options:favicon:file')
+        , faviconFile = path.join(twee.getBaseDirectory(), faviconFileName)
+        , favicon = require('serve-favicon');
+
+    if (twee.getConfig('twee:options:favicon:file') && fs.existsSync(faviconFile)) {
+        twee.getApplication().use(favicon(faviconFile));
+    }
+};
